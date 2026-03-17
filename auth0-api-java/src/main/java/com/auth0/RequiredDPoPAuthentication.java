792d5e8c2df232e35ea7c298990774926c3ef761
tanya732 <sinha.tanya26@gmail.com>
2026-03-17 10:49:22 +0530

    Updated comments and removed commented code

--- auth0-api-java/src/main/java/com/auth0/RequiredDPoPAuthentication.java
+++ auth0-api-java/src/main/java/com/auth0/RequiredDPoPAuthentication.java
@@ -19,7 +19,7 @@ class RequiredDPoPAuthentication extends AbstractAuthentication {
     }
 
     /**
-     * Authenticates the request when DPoP Mode is Allowed (Accepts only DPoP tokens) .
+     * Authenticates the request when DPoP Mode is Required (Accepts only DPoP tokens) .
      * @param requestInfo HTTP request info
      * @return AuthenticationContext with JWT claims
      * @throws BaseAuthException if validation fails
@@ -27,8 +27,6 @@ class RequiredDPoPAuthentication extends AbstractAuthentication {
     @Override
     public AuthenticationContext authenticate(HttpRequestInfo requestInfo)
             throws BaseAuthException {
-
-//        Map<String, String> normalizedHeader = normalize(requestInfo.getHeaders());
 
         try {
             DecodedJWT decodedJWT = validateDpopTokenAndProof(requestInfo);
